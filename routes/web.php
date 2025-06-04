eb56360f855394bf6837764127d5052d1d4d25ec
Shinta <ratnasarishinta008@gmail.com>
2025-06-04 22:57:05 +0800

    ta

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/resources/views/buku', function () {
+Route::get('/buku', function () {
     return view('buku');
 });
