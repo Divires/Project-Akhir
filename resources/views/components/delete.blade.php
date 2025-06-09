f6f6ed25ee7c434b912e399f4b04ec6b81355d63
Shinta <ratnasarishinta008@gmail.com>
2025-06-09 17:27:40 +0800

    dah

--- resources/views/components/delete.blade.php
+++ resources/views/components/delete.blade.php
@@ -40,13 +40,11 @@
                     Batal
                 </button>
 
-            <form method="POST" action="{{ $action }}">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-lg text-sm">
-                    Hapus
-                </button>
-            </form>
+                <form method="POST">
+                    <button type="submit" class="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-lg text-sm">
+                        Hapus
+                    </button>
+                </form>
             </div>
         </div>
     </div>
